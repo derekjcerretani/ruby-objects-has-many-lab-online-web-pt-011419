2225f4d23952d2ea92883503646c704295b7454a
Derek Cerretani <derekcerretani@gmail.com>
2019-02-11 17:20:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,9 +13,13 @@ def Author
   end
 
   def add_post_by_title(post_title)
-    post = Post.new
+    post = Post.new(post_title)
     @posts << post
-    
+    post.author = self
+  end
+
+  def self.post_count
+
   end
 
 end
